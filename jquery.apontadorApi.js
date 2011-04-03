889c039743db9b6028d99020ce1f98c82fd0ec25
Jair Henrique <jair.henrique@gmail.com>
2011-04-02 22:29:35 -0300

    Adicionado métodos para detalhes de local e busca de categorias

--- jquery.apontadorApi.js
+++ jquery.apontadorApi.js
@@ -27,6 +27,7 @@
 		};
 		
 		var methods = {
+			/* SEARCH */
 			getPlacesByPois : function( params, callback ) {
 				if ( typeof params !== 'object' ) { return false; }
 				var p = $.extend({}, params, _confBase);
@@ -56,7 +57,42 @@
 				if ( typeof params !== 'object' ) { return false; }
 				var p = $.extend({}, params, _confBase);
 				return _methods.doRequest("https://api.apontador.com.br/v1/suggestions?callback=?", p, callback );
+			},
+			
+			/* POIS */
+			getPlace : function( placeId, callback ) {
+				if( typeof placeId !== 'string' ) { return false; }
+				var p = $.extend({}, _confBase);
+				return _methods.doRequest("https://api.apontador.com.br/v1/places/"+placeId+"?callback=?", p, callback );
+			},
+			getPlacePhotos : function ( placeId, callback ) {
+				if( typeof placeId !== 'string' ) { return false; }
+				var p = $.extend({}, _confBase);
+				return _methods.doRequest("https://api.apontador.com.br/v1/places/"+placeId+"/photos?callback=?", p, callback );
+			},
+			getPlaceReviews : function( placeId, callback ) {
+				if( typeof placeId !== 'string' ) { return false; }
+				var p = $.extend({}, _confBase);
+				return _methods.doRequest("https://api.apontador.com.br/v1/places/"+placeId+"/reviews?callback=?", p, callback );
+			},
+			
+			/* Categorias */
+			getCategories : function ( params, callback ) {
+				if ( typeof params !== 'object' ) { params = {}; }
+				var p = $.extend({}, params, _confBase);
+				return _methods.doRequest("https://api.apontador.com.br/v1/categories/?callback=?", p, callback );
+			},
+			getSubCategoreis : function ( categoryId, params, callback ) {
+				if( typeof categoryId !== 'string' ) { return false; }
+				if( typeof params !== 'object' ) { params = {}; }
+				var p = $.extend({}, params, _confBase);
+				return _methods.doRequest("https://api.apontador.com.br/v1/categories/"+categoryId+"/subcategories?callback=?", p, callback );
+			},
+			getTopCategories : function( callback ) {
+				var p = {};
+				return _methods.doRequest("https://api.apontador.com.br/v1/categories/top?callback=?", p, callback );
 			}
+			
 		};
 		
 		return methods;
